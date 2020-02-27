cbbc05a9d745ec30ead849caba7db74208ec4caa
govindprajapati <govind.prajapati@solutionanalysts.com>
2020-02-27 18:36:44 +0530

    changes

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 import { AppRegistry } from 'react-native';
 import React from 'react';
 import { name as appName } from './app.json';
-import { Provider } from 'react-redux';
-import configureStore from './src/store/configStore';
 import App from './App.js';
 
 AppRegistry.registerComponent(appName, () => App);
